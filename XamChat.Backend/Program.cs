c80add5e9e3e6de42e516efcbd01fbb03f6fa7e7
G Key <g.key0325@gmail.com>
2024-01-11 17:48:53 -0800

    first

---
+++ XamChat.Backend/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace XamChat.Backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+           .ConfigureLogging(logging =>
+           {
+               logging.ClearProviders();
+               logging.AddConsole();
+           })
+           .ConfigureWebHostDefaults(webBuilder =>
+           {
+               webBuilder.ConfigureKestrel((context, options) =>
+               {
+#if DEBUG
+                   options.Listen(IPAddress.Loopback, 5000);
+#endif
+               })
+               .UseStartup<Startup>();
+           });
+    }
+}
---
+++ XamChat.Console/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using XamChat.Core;
+using XamChat.Core.EventHandlers;
+
+namespace XamChat.ConsoleApp
+{
+    public class Program
+    {
+        static ChatService service;
+        static string room;
+        static string name;
+        static Random random = new Random();
+        public static async Task Main(string[] args)
+        {
+            name = "console " + random.Next(0, 10000);
+            service = new ChatService();
+            service.OnReceivedMessage += Service_OnReceivedMessage;
+            Console.WriteLine("Enter IP:");
+            var ip = Console.ReadLine();
+            service.Init(ip, ip != "localhost");
+
+            await service.ConnectAsync();
+            Console.WriteLine("You are connected...");
+
+            await JoinRoom();
+
+            var keepGoing = true;
+            do
+            {
+                var text = Console.ReadLine();
+                if (text == "exit")
+                {
+                    keepGoing = false;
+                }
+                else if(text == "leave")
+                {
+                    await service.LeaveChannelAsync(room, name);
+                    await JoinRoom();
+                }
+                else
+                {
+                    await service.SendMessageAsync(room, name, text);
+                }
+            }
+            while (keepGoing);
+        }
+
+        static async Task JoinRoom()
+        {
+            Console.WriteLine($"Enter room ({string.Join(",", service.GetRooms())}):");
+            room = Console.ReadLine();
+
+            await service.JoinChannelAsync(room, name);
+        }
+
+
+
+        private static void Service_OnReceivedMessage(object sender, MessageEventArgs e)
+        {
+            if (e.User == name)
+                return;
+            Console.WriteLine(e.Message);
+        }
+    }
+}
